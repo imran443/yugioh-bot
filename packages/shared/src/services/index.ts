c74dff508b19f9bf2c96ee7ea798d1c465a4c944
imran443 <qureshi.443@gmail.com>
2026-05-04 19:36:36 -0400

    feat: add players service with findOrCreate and findByGuildAndUser

--- packages/shared/src/services/index.ts
+++ packages/shared/src/services/index.ts
@@ -3,3 +3,5 @@ export * from "./card-images.js";
 export * from "./drafts.js";
 export { createGuildSettingsService } from "./guild-settings.js";
 export type { GuildSettingsService, GuildSettings } from "./guild-settings.js";
+export { createPlayerService } from "./players.js";
+export type { PlayerService, Player } from "./players.js";
